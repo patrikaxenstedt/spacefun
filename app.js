5cf67a232a0006483f25a1312306e6798b13349f
patrikaxenstedt <patrik.eriksson.axenstedt@chasacademy.se>
2020-05-13 18:09:31 +0200

    upload project

---
+++ app.js
@@ -0,0 +1,49 @@
+let objImage = null;
+
+window.onload = init;
+
+// Spaceship
+function init() {
+  objImage = document.getElementById("image1");
+  objImage.style.position = "relative";
+  objImage.style.left = "525px";
+  objImage.style.top = "500px";
+}
+
+// För att styra Spaceship
+
+function getKeyAndMove(e) {
+  let key_code = e.which || e.keyCode;
+  switch (key_code) {
+    case 37: //vänster
+      moveLeft();
+      break;
+    case 38: //upp
+      moveUp();
+      break;
+    case 39: //höger
+      moveRight();
+      break;
+    case 40: //ned
+      moveDown();
+      break;
+  }
+}
+function moveLeft() {
+  objImage.style.left = parseInt(objImage.style.left) - 25 + "px";
+}
+function moveUp() {
+  objImage.style.top = parseInt(objImage.style.top) - 25 + "px";
+}
+function moveRight() {
+  objImage.style.left = parseInt(objImage.style.left) + 25 + "px";
+}
+function moveDown() {
+  objImage.style.top = parseInt(objImage.style.top) + 25 + "px";
+}
+// Spacebar för att skjuta (bara ljud())
+document.addEventListener("keydown", function (e) {
+  if (e.keyCode == 32) {
+    document.getElementById("audio").play();
+  }
+});
